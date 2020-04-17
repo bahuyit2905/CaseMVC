a6d0f2ab2e7cb4eae0a406635707c47149ee134b
bahuynguyen19972905 <bahuynguyen19972905@gmail.com>
2020-04-17 10:01:44 +0700

    crud

---
+++ Asp.netCoreMVCCRUD/Asp.netCoreMVCCRUD/Migrations/20200212034936_init3.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Asp.netCoreMVCCRUD.Migrations
+{
+    public partial class init3 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "nvarchar(100)",
+                table: "Employees",
+                newName: "Fullname");
+
+            migrationBuilder.RenameColumn(
+                name: "varchar(150)",
+                table: "Employees",
+                newName: "EmpCode");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Fullname",
+                table: "Employees",
+                type: "nvarchar(100)",
+                nullable: false,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "EmpCode",
+                table: "Employees",
+                type: "varchar(150)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "OfficeLocation",
+                table: "Employees",
+                type: "varchar(150)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Position",
+                table: "Employees",
+                type: "varchar(150)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "OfficeLocation",
+                table: "Employees");
+
+            migrationBuilder.DropColumn(
+                name: "Position",
+                table: "Employees");
+
+            migrationBuilder.RenameColumn(
+                name: "Fullname",
+                table: "Employees",
+                newName: "nvarchar(100)");
+
+            migrationBuilder.RenameColumn(
+                name: "EmpCode",
+                table: "Employees",
+                newName: "varchar(150)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "nvarchar(100)",
+                table: "Employees",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "varchar(150)",
+                table: "Employees",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(150)",
+                oldNullable: true);
+        }
+    }
+}
---
+++ Asp.netCoreMVCCRUD/Asp.netCoreMVCCRUD/Models/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asp.netCoreMVCCRUD.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int EmployeeId { get; set; }
+        [Column(TypeName = "nvarchar(100)")]
+        [Required(ErrorMessage ="Nhập thông tin bắt buộc")]
+        [DisplayName("Full name")]
+        public string Fullname { get; set; }
+        [Column(TypeName = "varchar(150)")]
+        [DisplayName("Emp.Code")]
+        public string EmpCode { get; set; }
+        [Column(TypeName = "varchar(150)")]
+        
+        public string Position { get; set; }
+        [Column(TypeName = "varchar(150)")]
+        [DisplayName("Office Location")]
+
+        public string OfficeLocation { get; set; }
+    }
+}
